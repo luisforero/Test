c5816113159c2cb222a2abec393483bbb89520b0
luisforero <luisforero.wr@gmail.com>
2016-12-26 10:05:46 -0500

    Generic lambda expression.

--- Test-ejb/src/main/java/co/com/examples/java8/generics/MainGenerics.java
+++ Test-ejb/src/main/java/co/com/examples/java8/generics/MainGenerics.java
@@ -9,6 +9,7 @@ import co.com.examples.java8.animals.Condor;
 import co.com.examples.java8.animals.Elephant;
 import co.com.examples.java8.animals.HeavyAnimal;
 import co.com.examples.java8.animals.Hippo;
+import co.com.examples.java8.lambda.Animal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,32 +19,32 @@ import java.util.List;
  * @author Luis Wilmer Forero Romero<luis.forero@tecnocom.biz>
  */
 public class MainGenerics {
-    
+
     static class OtherStatic {
-        
+
         private final String hi = "Hi";
-        
+
         @Override
         public String toString() {
             return hi;
         }
     }
-    
+
     public static void main(String[] args) {
         Crate<HeavyAnimal> elephantCrate = new Crate<>();
         elephantCrate.packCrate(new Elephant());
-        
+
         List<SizeLimitedCrate<HeavyAnimal, Integer>> list = new ArrayList<>();
         list.add(new SizeLimitedCrate<>(elephantCrate.emptyCrate(), 100));
         list.add(new SizeLimitedCrate<>(new Hippo(), 20));
         //list.add(new SizeLimitedCrate<>(new Condor(), 100));
         System.err.println(list.toString());
-        
+
         List<OtherStatic> listOtherStatic = new ArrayList<>();
         listOtherStatic.add(new OtherStatic());
-        
+
         System.out.println(listOtherStatic.get(0).hi);
-        
+
         OtherStatic[][] others = {{new OtherStatic()}};
         System.out.println(Arrays.toString(others));
 
@@ -51,8 +52,23 @@ public class MainGenerics {
         // OneGeneric<int> oneGeneric=new OneGeneric<>(123);
         // System.out.println(oneGeneric.getNumeric());
         int[] array = {1, 2, 3};
-        OneGeneric<Integer> oneGeneric = new OneGeneric<>(123, new ArrayList<Integer>(Arrays.asList(array)));
-        // System.out.println(oneGeneric.getNumeric());
-        
+        try {
+            // This is not an error compilation, is a runtime time error 
+            OneGeneric<Integer> oneGeneric = new OneGeneric<>(123, new ArrayList<Integer>(Arrays.asList(array)));
+            System.out.println(oneGeneric.getNumeric().toString());
+        } catch (RuntimeException ex) {
+            System.err.println(ex.getMessage() + "\nCause:" + ex.getCause());
+        }
+
+        System.out.println("******** Lambda Expression Execution ********");
+        System.out.println(doExpressionLambda(new Animal("Elephant", true, true), a -> a.toString()));
+    }
+    // ** ERROR IMPLEMENTATION BECOUSE MISSING <E> TO GENERIC METHOD
+//    private static String doExpressionLambda(E a, GenericLambda generic){
+//        return generic.expression(a);
+//    }
+
+    private static <E> String doExpressionLambda(E a, GenericLambda generic) {
+        return generic.expression(a);
     }
 }
