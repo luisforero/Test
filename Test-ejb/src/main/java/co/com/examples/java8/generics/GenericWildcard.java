1d238be4fdef2870fc990ba7b60d172f5f71a0c5
luisforero <luisforero.wr@gmail.com>
2016-12-26 17:00:06 -0500

    Bad use of a Wildcard

---
+++ Test-ejb/src/main/java/co/com/examples/java8/generics/GenericWildcard.java
@@ -0,0 +1,12 @@
+package co.com.examples.java8.generics;
+
+/**
+ * The wild card is not allowed in Java fic). The wildcard is never used as a
+ * type argument for a generic method invocation, a generic class instance
+ * creation, or a supertype.
+ *
+ * @author Luis Wilmer Forero Romero<luis.forero@tecnocom.biz>
+ */
+public class GenericWildcard<? extends Animal> {
+    
+}
