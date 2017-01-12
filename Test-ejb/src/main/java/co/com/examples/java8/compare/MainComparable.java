1d03ca85a28da5ab8dd474b7aa46cb02b4fc669b
luisforero <luisforero.wr@gmail.com>
2017-01-12 12:33:04 -0500

    Comparable

--- Test-ejb/src/main/java/co/com/examples/java8/compare/MainComparable.java
+++ Test-ejb/src/main/java/co/com/examples/java8/compare/MainComparable.java
@@ -5,12 +5,23 @@
  */
 package co.com.examples.java8.compare;
 
+import co.com.examples.java8.animals.Duck;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  *
  * @author Luis Wilmer Forero Romero<luisforero.wr@gmail.com>
  */
 public class MainComparable {
     public static void main(String[] args) {
-        
+        List<Duck> lstDucks= new ArrayList<>();
+        lstDucks.add(new Duck(5));
+        lstDucks.add(new Duck(1));
+        lstDucks.add(new Duck(3));
+        lstDucks.add(new Duck(2));
+        Collections.sort(lstDucks);
+        System.out.println("Ducks List: "+lstDucks);
     }
 }
